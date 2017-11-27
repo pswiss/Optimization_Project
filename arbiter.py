fe2bf426b0994b4a0afcd275e14191a0371360bd
pswiss <petrasswissler2022@u.northwestern.edu>
2017-11-26 23:42:58 -0600

    Updated Config values (1 run per phenotype)

--- arbiter.py
+++ arbiter.py
@@ -3,13 +3,13 @@
 length = 4;				#number of phenotypes per individual
 count = 2;			#number of individuals per population
 target = 3.5;			#target number for the fitness function
-retain_value = 0.5		#percentage of how many of these we retain
+retain_value = 0.1		#percentage of how many of these we retain
 random_select = 0.05	#random diversity value
-mutate_value = 0.01		#random mutation value
+mutate_value = 0.1		#random mutation value
 diversityRange = 2
 populationNumber = 200
 mutationScale = 0.5
 generationsNumber = 100
 
-numSimPerFit = 5
+numSimPerFit = 1
 genReportFile = 'generationTracker.txt'
