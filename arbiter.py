b43c6e83cffbac61475100298bf6b426f3ab374a
pswiss <petrasswissler2022@u.northwestern.edu>
2017-11-27 18:46:50 -0600

    make faster

--- arbiter.py
+++ arbiter.py
@@ -7,9 +7,9 @@ retain_value = 0.1		#percentage of how many of these we retain
 random_select = 0.05	#random diversity value
 mutate_value = 0.1		#random mutation value
 diversityRange = 2
-populationNumber = 100
+populationNumber = 80
 mutationScale = 0.5
-generationsNumber = 100
+generationsNumber = 50
 
 numSimPerFit = 1
 genReportFile = 'generationTracker.txt'
